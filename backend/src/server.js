91e84528164eae4b96e84df3fb2b414ebd4cd37f
DracoRooks <gokulmittal008@gmail.com>
2025-12-24 20:36:34 +0530

    Feat: Codebase is deployable to production.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 // importing necessary modules
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 // importing all routes
 import authRoutes from "./routes/auth.route.js";
@@ -10,10 +11,19 @@ import messageRoutes from "./routes/message.route.js";
 const website = express();
 dotenv.config();
 const PORT = process.env.PORT || 2001;
+const __dirname = path.resolve();
 
 // all endpoints
 website.use("/api/auth", authRoutes);
 website.use("/api/message", messageRoutes);
+
+// production deployment
+if(process.env.NODE_ENV === "production") {
+    website.use(express.static(path.join(__dirname, "../frontend/dist")));
+    website.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+}
 
 // listening port
 website.listen(PORT, "0.0.0.0", err => {
