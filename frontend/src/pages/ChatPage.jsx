7d63b18b410998dbb6d97ac5a5a01d5e6cd1ef3b
DracoRooks <gokulmittal008@gmail.com>
2026-01-07 01:33:59 +0530

    Feat: Add basic ChatPage boilerplate.

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useChatStore } from "../store/useChatStore.js";
+import ProfileHeader from "../components/ProfileHeader.jsx";
+import ActiveTabSwitch from "../components/ActiveTabSwitch.jsx";
+import ChatList from "../components/ChatList.jsx";
+import ContactList from "../components/ContactList.jsx";
+import ChatContainer from "../components/ChatContainer.jsx";
+import ChatPlaceholder from "../components/ChatPlaceholder.jsx";
 
 function ChatPage() {
+  const { activeTab, activeChat } = useChatStore();
+
   return (
-    <div>
-      ChatPage
+    <div className="flex w-full max-w-6xl h-screen md:max-h-175 max-h-160 ">
+
+      {/* LEFT USER LIST PANEL */}
+      <div className="w-full max-w-[28%] bg-slate-800/50 flex flex-col backdrop-blur-sm rounded-xl">
+        <ProfileHeader />
+        <ActiveTabSwitch />
+
+        <div className="flex-1 overflow-y-auto space-y-2 p-4">
+          { activeTab === "chats" ? <ChatList /> : null }
+          { activeTab === "contacts" ? <ContactList /> : null }
+        </div>
+      </div>
+
+      {/* RIGHT CHAT PANEL */}
+      <div className="w-full max-w-[72%] flex flex-1 flex-col bg-slate-900/60 backdrop-blur-sm">
+        { activeChat ? <ChatContainer /> : <ChatPlaceholder /> }
+      </div>
     </div>
   );
 };
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set, get) => ({
+    allContacts: [],
+    allChats: [],
+    messages: [],
+    activeTab: "chats",
+    activeChat: null,
+    isContactsLoading: false,
+    isChatsLoading: false,
+    isMessagesLoading: false,
+    isAudioEnabled: localStorage.getItem("isAudioEnabled") === true,
+
+    setActiveTab: (tab) => set({ activeTab: tab }),
+
+    setActiveChat: (chat) => set({ activeChat: chat }),
+
+    audioToggle: () => {
+        localStorage.setItem("isAudioEnabled", !get().isAudioEnabled);
+        set({ isAudioEnabled: !get().isAudioEnabled });
+    },
+
+    getAllContacts: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/message/contacts");
+            set({ allContacts: res.data });
+
+        } catch (error) {
+            console.error("[ERROR]::USE_CHAT_STORE::GET_ALL_CONTACTS:", error);
+            toast.error(error.response?.data?.message || error);
+
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+
+    getAllChatPartners: async () => {
+        set({ isChatsLoading: true });
+        try {
+            const res = await axiosInstance.get("/message/chats");
+            set({ allChats: res.data });
+
+        } catch (error) {
+            console.error("[ERROR]::USE_CHAT_STORE::GET_ALL_CHATS:", error);
+            toast.error(error.response?.data?.message || error);
+
+        } finally {
+            set({ isChatsLoading: false });
+        }
+    },
+
+    getMessages: async () => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get("/message/" + get().activeChat);
+            set({ messages: res.data });
+
+        } catch (error) {
+            console.error("[ERROR]::USE_CHAT_STORE::GET_MESSAGES:", error);
+            toast.error(error.response?.data?.message || error);
+
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    },
+}));
